6f46c84e4b1ea7ffaad061a7d43c012a17716e4a
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2023-09-24 21:56:52 +0200

    Update background.js
    Fixed support for more than 2 tabs having sound
    Fixed processing when closing a tab

--- src/javascripts/background.js
+++ src/javascripts/background.js
@@ -6,10 +6,12 @@ let latestAudibleTabId = null;
 let actualAudibleTabId = null;
 
 const muteOtherTabs = (changedTab, ignoreList) => {
+console.log('- Start muteOtherTabs');
   chrome.tabs.query({ audible: true }, (tabs) => {
     tabs.forEach((tab) => {
       const onIgnoreList = ignoreList && Math.max(ignoreList.split(/\n/).map(i => tab.url.indexOf(i.trim()))) > -1;
       if (tab.id !== changedTab.id && !onIgnoreList) {
+		latestAudibleTabId = tab.id;
         chrome.tabs.update(tab.id, { muted: true });
       }
     })
@@ -17,23 +19,27 @@ const muteOtherTabs = (changedTab, ignoreList) => {
 };
 
 const unmuteRecentTab = () => {
-	
-	console.log('DEBUT unmuteRecentTab');
-	
+	console.log('- Start unmuteRecentTab');
+	console.log('- latestAudibleTabId : '+ latestAudibleTabId +' - ActualAudibleTabId : '+ actualAudibleTabId);
 	chrome.tabs.query({ muted: true }, (tabs) => {
 		tabs.forEach((tab) => {
-		
-		console.log(tab);
-		
-	//	const latestAudibleTabId = Math.max(tabs.map(tab => tab.id));
-		const latestAudibleTabId = tab.id;
-				
-		console.log('latestAudibleTabId : '+ latestAudibleTabId);
-		console.log('actualAudibleTabId : '+ actualAudibleTabId);
 		if (latestAudibleTabId !== actualAudibleTabId) {
 		chrome.tabs.update(latestAudibleTabId, { muted: false });
 		}
-		console.log('------- FIN unmuteRecentTab');  
+		console.log('- End unmuteRecentTab');  
+	});
+
+	})
+  
+};
+
+const unmuteRecentTabremoved = () => {
+	console.log('- Start unmuteRecentTabremoved');
+	console.log('- latestAudibleTabId : '+ latestAudibleTabId +' - ActualAudibleTabId : '+ actualAudibleTabId);
+	chrome.tabs.query({ muted: true }, (tabs) => {
+		tabs.forEach((tab) => {
+		chrome.tabs.update(latestAudibleTabId, { muted: false });
+		console.log('- End unmuteRecentTab');  
 	});
 
 	})
@@ -43,47 +49,33 @@ const unmuteRecentTab = () => {
 const tabUpdated = (tabId, changeInfo, tab) => {
   chrome.storage.local.get((storage) => {
     const { ignoreList } = storage;	
+	actualAudibleTabId = tabId;
+	console.log('- Start tabUpdated');
+	console.log('- latestAudibleTabId : '+ latestAudibleTabId +' - ActualAudibleTabId : '+ actualAudibleTabId);
     if (changeInfo && changeInfo.audible === true) {
       const onIgnoreList = ignoreList && Math.max(ignoreList.split(/\n/).map(i => tab.url.indexOf(i.trim()))) > -1;
       if (!onIgnoreList) {
-		console.log('--------------- not on ignor list - mute other tabs  ------------------');
-		actualAudibleTabId = tabId;
+		console.log('- Not on ignor list - muting other tabs');
         muteOtherTabs(tab, ignoreList);
       }
-	  	console.log('--------------- tabUpdated ------------------');
-		console.log('--------------- audible === true ------------------');
-		console.log('tab n°'+tabId + ' :  '+tab.title);
-		console.log(JSON.stringify(changeInfo))
-		console.log('audible : ' + JSON.stringify(tab.audible));
-		console.log(JSON.stringify(tab.mutedInfo))
-		console.log('actualAudibleTabId : '+ actualAudibleTabId);
-	   console.log(tab.mutedInfo.reason);
 	  
     }
     if (changeInfo && changeInfo.audible === false) {
       if (storage && storage.unmuteLastTab) {
-		console.log('--------------- tabUpdated ------------------');
-		console.log('--------------- audible === false ------------------');
-		console.log('tab n°'+tabId + ' :  '+tab.title);
-		console.log(JSON.stringify(changeInfo))
-		console.log('audible : ' + JSON.stringify(tab.audible));
-		console.log(JSON.stringify(tab.mutedInfo))
-		console.log('actualAudibleTabId : '+ actualAudibleTabId);
-        unmuteRecentTab();
+		console.log(' - latestAudibleTabId : '+ latestAudibleTabId +' - ActualAudibleTabId : '+ actualAudibleTabId);
+		unmuteRecentTab();
       }
     }
   });
-  actualAudibleTabId = tabId;
+  console.log('-End tabUpdated');
+ 
 };
 
 chrome.storage.local.get((storage) => {
   if (!storage.ignoreList) {
     chrome.storage.local.set(DEFAULT_STORAGE_DATA);
   }
- 	console.log('--------------- DEFAULT_STORAGE_DATA ------------------');
-	console.log(storage); 
-  
 });
 
 chrome.tabs.onUpdated.addListener(tabUpdated);
-chrome.tabs.onRemoved.addListener(unmuteRecentTab);
+chrome.tabs.onRemoved.addListener(unmuteRecentTabremoved);
