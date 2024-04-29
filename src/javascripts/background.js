985a794d87e60e7728ce09e5e06c4f959985ef5d
Tonton Jo <60965766+Tontonjo@users.noreply.github.com>
2024-04-29 17:39:31 +0200

    Add files via upload

--- src/javascripts/background.js
+++ src/javascripts/background.js
@@ -1,5 +1,5 @@
 const DEFAULT_STORAGE_DATA = {
-  ignoreList: 'messenger.com\nslack.com\noutlook.com',
+  ignoreList: 'messenger.com\nslack.com\noutlook.com\noffice.com',
   whitelist: 'voipsystem.com',
   unmuteLastTab: true,
 }
@@ -16,12 +16,12 @@ const muteOtherTabs = (changedTab, whitelist) => {
         console.log("- new tab domain: ", fqdn);
         // Comparaison avec la liste ignoreList
         if (whitelist.includes(fqdn)) {
-          console.log('- On ignore list - dont mute');
+          console.log('- On whitelist - dont mute');
         } else {
-          console.log(whitelist);
-          console.log('- Not on ignore list - muting other tabs');
-	  latestAudibleTabId = tab.id;
+          console.log('- Not on whitelist - muting tab');
+		  latestAudibleTabId = tab.id;
           chrome.tabs.update(tab.id, { muted: true });
+		  console.log('- End muteOtherTabs');
         }
       }
     });
@@ -37,35 +37,56 @@ const unmuteRecentTab = () => {
 
 const unmuteRecentTabremoved = (changeInfo) => {
 	console.log('- Start unmuteRecentTabremoved');
+	if (latestAudibleTabId !== null) {
+    // Perform your action here
+    console.log("- latestAudibleTabId is not null, unmuting latest known tab");
+	chrome.tabs.update(latestAudibleTabId, { muted: false });
+} else {
+    console.log("- latestaudibletabid is null.");
 	if (changeInfo.audible === true) {
 	console.log('- latestAudibleTabId : '+ latestAudibleTabId +' - ActualAudibleTabId : '+ actualAudibleTabId);
 	chrome.tabs.update(latestAudibleTabId, { muted: false });
 	actualAudibleTabId = latestAudibleTabId
 	}
+}
+
+
 	console.log('- End unmuteRecentTab');  
 };
 
 const unmuteselectedtab = () => {
-console.log('- Start unmuteselectedtab');
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.get(activeTab.id, function(tab) {
-      if (tab.mutedInfo && tab.mutedInfo.muted) {
-        chrome.tabs.update(activeTab.id, { muted: false });
-      }
-	  });
+  console.log('- Start of unmuteselectedtab');
+  chrome.storage.local.get((storage) => {
+    const whitelist = storage.whitelist;
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      const activeTab = tabs[0];
+      chrome.windows.getCurrent(function(window) {
+        if (window.focused) {
+          chrome.tabs.get(activeTab.id, function(tab) {
+            if (tab.mutedInfo && tab.mutedInfo.muted) {
+              chrome.tabs.update(activeTab.id, { muted: false });
+              muteOtherTabs(tab, whitelist);
+            }
+          });
+        }
+      });
+    });
+    console.log('- End of unmuteselectedtab');
   });
-console.log('- End unmuteselectedtab');
 };
 
+
+
 const tabUpdated = (tabId, changeInfo, tab) => {
+  let actualAudibleTabId = tabId;
+  if (latestAudibleTabId !== actualAudibleTabId) {
   if (changeInfo && (changeInfo.audible === true || changeInfo.audible === false)) {
     chrome.storage.local.get((storage) => {
-      let actualAudibleTabId = tabId; // Déclarez actualAudibleTabId avec "let" pour éviter une erreur
+      let actualAudibleTabId = tabId;
       console.log('- Start tabUpdated');
       console.log('- latestAudibleTabId : ' + latestAudibleTabId + ' - ActualAudibleTabId : ' + actualAudibleTabId);
-
       if (changeInfo && changeInfo.audible === true) {
+		console.log('- Tab is audible');  
         browser.tabs.query({ active: true, currentWindow: true })
           .then((tabs) => {
             if (tabs.length > 0) {
@@ -74,31 +95,27 @@ const tabUpdated = (tabId, changeInfo, tab) => {
               const currentTab = tabs[0];
               const currentUrl = new URL(currentTab.url);
               const fqdn = currentUrl.hostname;
-              console.log("- new tab domain: ", fqdn);
-              // Comparaison avec la liste ignoreList
+              console.log("- New tab domain: ", fqdn);
               if (ignoreList.includes(fqdn)) {
-                console.log('- On ignore list - tab wont get muted');
+                console.log('- On ignorelist - tab wont get muted');
               } else {
-                console.log(whitelist);
-                console.log('- Not on ignore list - muting other tabs');
+                console.log('- Not on ignoreList - muting other tabs');
                 muteOtherTabs(tab, whitelist);
               }
             }
           });
-      }
-
-      if (changeInfo && changeInfo.audible === false) {
+      } else {
+		console.log('- Tab is not audible');
         if (storage && storage.unmuteLastTab) {
-          if (latestAudibleTabId !== actualAudibleTabId) {
-            unmuteRecentTab();
-          }
+		unmuteRecentTab();
         }
-      }
-
+	  }
       console.log('- End tabUpdated');
     });
   }
 };
+}
+
 
 
 chrome.storage.local.get((storage) => {
@@ -110,6 +127,11 @@ chrome.storage.local.get((storage) => {
   }
 });
 
+// Listener for changes in tabs
 chrome.tabs.onUpdated.addListener(tabUpdated);
+// Listener for tab removals
 chrome.tabs.onRemoved.addListener(unmuteRecentTabremoved);
+// Listener for tab selection
 chrome.tabs.onActivated.addListener(unmuteselectedtab)
+// Listener windows focus changes
+chrome.windows.onFocusChanged.addListener(unmuteselectedtab)
